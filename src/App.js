f1e355f848230bbd731336c740f6da1d3c88f604
Gaurav Chandak <gaurav01101993@gmail.com>
2021-02-28 15:54:54 +0530

    Update website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+  
+  function Bye(props) {
+    return <h1>Bye, {props.name}</h1>;
+  }
+  
+  function App() {
+    return (
+      <div className="app">
+        <Welcome name="Elon" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+        <Bye name="Elon" />
+      </div>
+    );
+  }
 
 export default App;
